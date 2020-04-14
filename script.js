2ed79774774699d517258cc97b315138d074b039
Dima <dima22kolis@ukr.net>
2020-04-14 11:04:48 +0300

    add 3 types of cycle

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let money = prompt("Ваш бюджет на месяц?");
+let money = +prompt("Ваш бюджет на месяц?");
 let time = prompt("Введите дату в формате YYYY-MM-DD");
 console.log(money);
 console.log(time);
@@ -15,17 +15,68 @@ let appData = {
 };
 
 
-let answer = prompt("Введите обязательную статью расходов в этом месяце");
-let answer2 = prompt("Во сколько обойдется?");
 
-appData.expenses[answer] = answer2;
+
+
+// appData.expenses.answer = answer2;
+// appData.expenses.answer3= answer4;
+
 
 console.log(appData.expenses);
 
+// for(let i=0; i < 2; i++){
+//     let a = prompt("Введите обязательную статью расходов в этом месяце"),
+//     b = prompt("Во сколько обойдется?");
+    
+//     if(typeof(a)==='string' && typeof(a)!= null && typeof(b)!=null && a!= ' ' && b!= ' '&& a.length < 50){
+//         console.log("done");
+//         appData.expenses [a] = b;
+//     }else{
+//         console.log("вы ввели не строку!!!");
 
-let moneyOneDay = money/30;
-alert("Ваш расход на 1 день составляет  " + moneyOneDay + "грн");
-console.log(moneyOneDay);
+//     }
+// }
+let i = 0;
+while(i < 2){
+    let a = prompt("Введите обязательную статью расходов в этом месяце"),
+    b = prompt("Во сколько обойдется?");
+    
+    if(typeof(a)==='string' && typeof(a)!= null && typeof(b)!=null && a!= ' ' && b!= ' '&& a.length < 50){
+        console.log("done");
+        appData.expenses [a] = b;
+    }else{
+        //домашка
+    }
+    i++;
+}
+
+
+// let i = 0;
+// do{
+//     let a = prompt("Введите обязательную статью расходов в этом месяце"),
+//     b = prompt("Во сколько обойдется?");
+    
+//     if(typeof(a)==='string' && typeof(a)!= null && typeof(b)!=null && a!= ' ' && b!= ' '&& a.length < 50){
+//         console.log("done");
+//         appData.expenses [a] = b;
+//     }else{
+//         //домашка
+//     }
+// i++;
+// }while(i < 2)
+
+
+appData.moneyPerDay = appData.moneyData/30;
+
+alert("бабки на день " + appData.moneyPerDay);
+
+if (appData.moneyPerDay <= 100){
+    console.log("мало");
+} else if (appData.moneyPerDay > 100) {
+    console.log("достаточно, но лучше больше");
+} else {
+    console.log("чем больше тем лучше");
+}
 
 
 /*
